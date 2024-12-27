cc553bfebf540371138a37ad9dbfc0e91510b827
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2024-12-27 18:22:35 +0100

    Upd SASS and add base template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new Response('Salut les gens !');
+        return $this->render('home/index.html.twig');
     }
 }
